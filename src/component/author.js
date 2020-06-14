263e974d5c2b308061f0b6646024c0f95383c7a6
yasicarajesh <yasicarajesh@gmail.com>
2020-06-14 18:26:37 +0530

    author

---
+++ src/component/author.js
@@ -0,0 +1,17 @@
+import React from 'react';
+class Author extends React.Component {
+    render() {
+        return (
+            <> 
+
+            <div class="row">
+            <div class="col-xs-12 col-sm-offset-3 col-sm-6">
+                <div class="panel panel-default">
+                <div class="panel-heading c-list">
+                <img src="auth/MayPamintuan.jpg" class="team"/>
+                </div></div></div></div>
+                </>
+                );
+        }
+    }
+    export default Author;
